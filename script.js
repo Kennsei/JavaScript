2dfabf6f63f4a94bd2892377ab17848958cea99c
Kennsei <84730591+Kennsei@users.noreply.github.com>
2021-06-16 22:13:33 -0300

    Rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleEntrar() {
+  var confirmPassword=document.getElementById("confirmPassword")
+  confirmPassword.style.display="none"
+  
+  
+}
+function toggleRegistrar() {
+  var confirmPassword=document.getElementById("confirmPassword")
+  confirmPassword.style.display="block"
+  
+  
+}
+
+
